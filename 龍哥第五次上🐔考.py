0ac1fc1d2a7651154142f0679c6927da038a6ec4
Yi-Chung-Chen <94212671+Yi-Chung-Chen@users.noreply.github.com>
2022-05-04 22:53:38 +0800

    Update 龍哥第五次上🐔考.py

--- 龍哥第五次上🐔考.py
+++ 龍哥第五次上🐔考.py
@@ -12,100 +12,38 @@ def getMatrixDeternminant(m):
     return determinant
 
 
-matrix=input().split()
+def slicing(matrix):
+  if len(matrix)==4:
+    new_matrix = [matrix[i:i+2] for i in range(0,len(matrix),2)]
+  elif len(matrix)==9:
+    new_matrix = [matrix[i:i+3] for i in range(0,len(matrix),3)]
+  elif len(matrix)==16:
+    new_matrix = [matrix[i:i+4] for i in range(0,len(matrix),4)]
+  elif len(matrix)==25:
+    new_matrix = [matrix[i:i+5] for i in range(0,len(matrix),5)]
+  return new_matrix
 
-def modification(matrix):
-    if len(matrix)<4:
-        for i in range(4-len(matrix)):
-            matrix.append(0)
-            break
-    elif len(matrix)<9:
-        for j in range(9-(len(matrix))):
-            matrix.append(0)
-            break
-    elif len(matrix)<16:
-        for k in range(16-(len(matrix))):
-            matrix.append(0)
-            break
-    elif len(matrix)<25:
-        for l in range(25-(len(matrix))):
-            matrix.append(0)
-            break
-    return matrix
 
-matirx = modification(matrix)
+matrix = input().split()
+if len(matrix)==3 or len(matrix)==8 or len(matrix)==15 or len(matrix)==24:
+  matrix.append(0)
+elif len(matrix)==7 or len(matrix)==14 or len(matrix)==23:
+  matrix.append(0)
+  matrix.append(0)
+elif  len(matrix)==13 or len(matrix)==22:
+  matrix.append(0)
+  matrix.append(0)
+  matrix.append(0)
+elif len(matrix)==21:
+  matrix.append(0)
+  matrix.append(0)
+  matrix.append(0)
+  matrix.append(0)
 
-if len(matrix)==4:
-    a=[]
-    b=[]
-    c=[]
-    for i in range(2):
-        a.append(int(matrix[i]))
-    for j in range(2,4):
-        b.append(int(matrix[j]))
-    c.append(a)
-    c.append(b)
-    print(getMatrixDeternminant(c))
-    
-elif len(matrix)==9:
-    a=[]
-    b=[]
-    c=[]
-    d=[]
+matrix = [int(i) for i in matrix]
 
-    for i in range(3):
-        a.append(int(matrix[i]))
-    for j in range(3,6):
-        b.append(int(matrix[j]))
-    for k in range(6,9):
-        c.append(int(matrix[k]))
-    d.append(a)
-    d.append(b)
-    d.append(c)
-    print(getMatrixDeternminant(d))
-elif len(matrix)==16:
-    a=[]
-    b=[]
-    c=[]
-    d=[]
-    e=[]
+new = slicing(matrix)
+print(getMatrixDeternminant(new))
 
-    for i in range(4):
-        a.append(int(matrix[i]))
-    for j in range(4,8):
-        b.append(int(matrix[j]))
-    for k in range(8,12):
-        c.append(int(matrix[k]))
-    for l in range(12,16):
-        d.append(int(matrix[l]))
-
-    e.append(a)
-    e.append(b)
-    e.append(c)
-    e.append(d)
-    print(getMatrixDeternminant(e))
-elif len(matrix)==25:
-    a=[]
-    b=[]
-    c=[]
-    d=[]
-    e=[]
-    f=[]
-    for i in range(5):
-        a.append(int(matrix[i]))
-    for j in range(5,10):
-        b.append(int(matrix[j]))
-    for k in range(10,15):
-        c.append(int(matrix[k]))
-    for l in range(15,20):
-        d.append(int(matrix[l]))
-    for m in range(20,25):
-        e.append(int(matrix[m]))
-    f.append(a)
-    f.append(b)
-    f.append(c)
-    f.append(d)
-    f.append(e)
-    print(getMatrixDeternminant(f))
 #上面用的function 來自https://stackoverflow.com/questions/3819500/code-to-solve-determinant-using-python-without-using-scipy-linalg-det
 #下方的coding神人 
